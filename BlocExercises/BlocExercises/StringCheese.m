b7201f75f1cdbd54219669862295a21aed2382eb
dallas-matthews <matthews.dallas@gmail.com>
2015-11-12 22:34:15 -0500

    Update Completed Assignment 9 Strings

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,57 +12,36 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    
     NSString *newCheese = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
     
     return newCheese;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
+    
+    NSString *newCheeseName = cheeseName;
     if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
         
-        NSRange cheeseRange = [cheeseName rangeOfString:cheeseName options:NSCaseInsensitiveSearch];
-        NSString *subString = [cheeseName substringWithRange:cheeseRange];
-        NSString *newCheeseName = [subString stringByReplacingOccurrencesOfString:@" cheese" withString:@""];
-        
-        return newCheeseName;
-        
-    } else {
-        /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
-        
-        return cheeseName;
+        NSRange cheeseRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+        newCheeseName = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
     }
-
-    /*
-     (You will learn more about if/else statements in a later checkpoint.)
-     */
     
-    
-
+    return newCheeseName;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
+    NSMutableString *mutCheese;
     if (cheeseCount == 1) {
-        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
-        
-        NSMutableString *mutCheese = [NSMutableString stringWithFormat:@"%lu", (unsigned long)cheeseCount];
+        mutCheese = [NSMutableString stringWithFormat:@"%lu", (unsigned long)cheeseCount];
         [mutCheese appendString:@" cheese"];
-        
-        return mutCheese;
         
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
         
-        NSMutableString *mutCheese = [NSMutableString stringWithFormat:@"%lu", (unsigned long)cheeseCount];
+        mutCheese = [NSMutableString stringWithFormat:@"%lu", (unsigned long)cheeseCount];
         [mutCheese appendString:@" cheeses"];
-        
-        return mutCheese;
     }
-    
-    /*
-     (You will learn more about if/else statements in a later checkpoint.)
-     */
-    return nil;
+    return mutCheese;
 }
 
 @end
